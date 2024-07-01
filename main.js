b2c8fac514031b54c083c8d61c66ff418b078d2a
lnsvr <156704662+lnsvr@users.noreply.github.com>
2024-07-01 10:13:53 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,233 @@
+let totalPrice = 0;
+
+
+$('.pizzaItem').hide(0);
+$('#fetaGroup').click(function () {
+    $('#fetaGroup').hide(100);
+    $('#fetaHide').show(100);
+    totalPrice += 30;
+    
+})
+
+$('#fetaHide').click(function () {
+    $('#fetaGroup').show(100);
+    $('#fetaHide').hide(100);
+})
+
+
+
+
+
+
+
+
+$('.pizzaItem').hide(0);
+$('#mozzarellaGroup').click(function(){
+  
+    $('#mozzarellaGroup').hide(100);
+    $('#mozzarellaHide').show(100);
+})
+$('#mozzarellaHide').click(function(){
+    $('#mozzarellaGroup').show(100);
+    $('#mozzarellaHide').hide(100);
+})
+
+
+
+
+$('.pizzaItem').hide(0);
+$('#cheddarGroup').click(function(){
+  
+    $('#cheddarGroup').hide(100);
+    $('#cheddarHide').show(100);
+})
+$('#cheddarHide').click(function(){
+    $('#cheddarGroup').show(100);
+    $('#cheddarHide').hide(100);
+})
+
+
+
+$('.pizzaItem').hide(0);
+$('#bluGroup').click(function(){
+  
+    $('#bluGroup').hide(100);
+    $('#bluHide').show(100);
+})
+$('#bluHide').click(function(){
+    $('#bluGroup').show(100);
+    $('#bluHide').hide(100);
+})
+
+
+
+$('.pizzaItem').hide(0);
+$('#parmezanGroup').click(function(){
+  
+    $('#parmezanGroup').hide(100);
+    $('#parmezanHide').show(100);
+})
+$('#parmezanHide').click(function(){
+    $('#parmezanGroup').show(100);
+    $('#parmezanHide').hide(100);
+})
+
+
+
+$('.pizzaItem').hide(0);
+$('#bekonGroup').click(function(){
+  
+    $('#bekonGroup').hide(100);
+    $('#bekonHide').show(100);
+})
+$('#bekonHide').click(function(){
+    $('#bekonGroup').show(100);
+    $('#bekonHide').hide(100);
+})
+
+
+$('.pizzaItem').hide(0);
+$('#paparoniGroup').click(function(){
+  
+    $('#paparoniGroup').hide(100);
+    $('#paparoniHide').show(100);
+})
+$('#paparoniHide').click(function(){
+    $('#paparoniGroup').show(100);
+    $('#paparoniHide').hide(100);
+})
+
+
+$('.pizzaItem').hide(0);
+$('#shunkaGroup').click(function(){
+  
+    $('#shunkaGroup').hide(100);
+    $('#shunkaHide').show(100);
+})
+$('#shunkaHide').click(function(){
+    $('#shunkaGroup').show(100);
+    $('#shunkaHide').hide(100);
+})
+
+
+$('.pizzaItem').hide(0);
+$('#chickenGroup').click(function(){
+  
+    $('#chickenGroup').hide(100);
+    $('#chickenHide').show(100);
+})
+$('#chickenHide').click(function(){
+    $('#chickenGroup').show(100);
+    $('#chickenHide').hide(100);
+})
+
+
+
+$('.pizzaItem').hide(0);
+$('#barbequGroup').click(function(){
+  
+    $('#barbequGroup').hide(100);
+    $('#barbequHide').show(100);
+})
+$('#barbequHide').click(function(){
+    $('#barbequGroup').show(100);
+    $('#barbequHide').hide(100);
+})
+
+
+
+$('.pizzaItem').hide(0);
+$('#chasnykGroup').click(function(){
+  
+    $('#chasnykGroup').hide(100);
+    $('#chasnykHide').show(100);
+})
+$('#chasnykHide').click(function(){
+    $('#chasnykGroup').show(100);
+    $('#chasnykHide').hide(100);
+})
+
+
+$('.pizzaItem').hide(0);
+$('#alfredoGroup').click(function(){
+  
+    $('#alfredoGroup').hide(100);
+    $('#alfredoHide').show(100);
+})
+$('#alfredoHide').click(function(){
+    $('#alfredoGroup').show(100);
+    $('#alfredoHide').hide(100);
+})
+
+
+$('.pizzaItem').hide(0);
+$('#ananasGroup').click(function(){
+  
+    $('#ananasGroup').hide(100);
+    $('#ananasHide').show(100);
+})
+$('#ananasHide').click(function(){
+    $('#ananasGroup').show(100);
+    $('#ananasHide').hide(100);
+})
+
+
+$('.pizzaItem').hide(0);
+$('#grubGroup').click(function(){
+  
+    $('#grubGroup').hide(100);
+    $('#grubHide').show(100);
+})
+$('#grubHide').click(function(){
+    $('#grubGroup').show(100);
+    $('#grubHide').hide(100);
+})
+
+
+$('.pizzaItem').hide(0);
+$('#kukurudzaGroup').click(function(){
+  
+    $('#kukurudzaGroup').hide(100);
+    $('#kukurudzaHide').show(100);
+})
+$('#kukurudzaHide').click(function(){
+    $('#kukurudzaGroup').show(100);
+    $('#kukurudzaHide').hide(100);
+})
+
+
+$('.pizzaItem').hide(0);
+$('#peretsGroup').click(function(){
+  
+    $('#peretsGroup').hide(100);
+    $('#peretsHide').show(100);
+})
+$('#peretsHide').click(function(){
+    $('#peretsGroup').show(100);
+    $('#peretsHide').hide(100);
+})
+
+
+
+$('.pizzaItem').hide(0);
+$('#shpunatGroup').click(function(){
+  
+    $('#shpunatGroup').hide(100);
+    $('#shpunatHide').show(100);
+})
+$('#shpunatHide').click(function(){
+    $('#shpunatGroup').show(100);
+    $('#shpunatHide').hide(100);
+})
+
+
+$('.pizzaItem').hide(0);
+$('#tomatoGroup').click(function(){
+  
+    $('#tomatoGroup').hide(100);
+    $('#tomatoHide').show(100);
+})
+$('#tomatoHide').click(function(){
+    $('#tomatoGroup').show(100);
+    $('#tomatoHide').hide(100);
+})
